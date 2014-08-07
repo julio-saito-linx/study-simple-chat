5add2271f41f73e589111ae396bd3f46bceec272
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-07 18:20:42 -0300

    jade template

--- enableMogger.js
+++ enableMogger.js
@@ -2,21 +2,38 @@ var Mogger = require("mogger");
 
 module.exports = function(express) {
 
-	var tracer = new Mogger.Tracer();
-
-	tracer.traceObj({
-	  before: { message: 'express: ' },
-	  target: express
+	var tracer = new Mogger.Tracer({
+		showPause: true,
+		interceptors: [
+			{
+				filterRegex: /./i,
+				callback: function(info) {
+					var methodName = info.method;
+					var args = info.args && info.args.length > 0 && info.args[0] && info.args[0].toString();
+					return methodName + '("' + args + '")';
+				}
+			}
+		],		
 	});
 
 	tracer.traceObj({
-	  before: { message: 'express.request: ' },
+	  before: { message: 'req:  ' },
 	  target: express.request
 	});
 
 	tracer.traceObj({
-	  before: { message: 'express.response: ' },
+	  before: { message: 'resp: ' },
 	  target: express.response
+	});
+
+	tracer.traceObj({
+	  before: { message: 'app : ' },
+	  target: express.application
+	});
+
+	tracer.traceObj({
+	  before: { message: 'rout: ' },
+	  target: express.Router.prototype
 	});
 
 };
