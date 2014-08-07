a479a24323e4ae3cbf0224183cd5a6adc78d66c9
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-07 18:34:53 -0300

    very simple, working but ugly

--- enableMogger.js
+++ enableMogger.js
@@ -4,16 +4,16 @@ module.exports = function(express) {
 
 	var tracer = new Mogger.Tracer({
 		showPause: true,
-		interceptors: [
-			{
-				filterRegex: /./i,
-				callback: function(info) {
-					var methodName = info.method;
-					var args = info.args && info.args.length > 0 && info.args[0] && info.args[0].toString();
-					return methodName + '("' + args + '")';
-				}
-			}
-		],		
+		// interceptors: [
+		// 	{
+		// 		filterRegex: /./i,
+		// 		callback: function(info) {
+		// 			var methodName = info.method;
+		// 			var args = info.args && info.args.length > 0 && info.args[0] && info.args[0].toString();
+		// 			return methodName + '("' + args + '")';
+		// 		}
+		// 	}
+		// ],		
 	});
 
 	tracer.traceObj({
--- server.js
+++ server.js
@@ -3,15 +3,27 @@ var app = express();
 var port = 3700;
 require('./enableMogger')(express);
 
+// jade tamplate
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
+// index/ root
 app.get("/", function(req, res){
-    res.render("page");
+  res.render("page");
 });
 
-app.listen(port);
+// static files
+app.use(express.static(__dirname + '/public'));
+
+// start and get socket.io
+var io = require('socket.io').listen(app.listen(port));
 
 console.log("Listening on port " + port);
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'welcome to the chat' });
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data);
+  });
+});
