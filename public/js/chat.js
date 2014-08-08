012bf4ca8c3e9fc7b03d14c5325299276e6b3ec9
Julio Makdisse Saito <saitodisse@gmail.com>
2014-08-07 23:55:48 -0300

    serverIP configured in public/js/configuration.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,43 +1,45 @@
 $(document).ready(function() {
-    $("#field").keyup(function(e) {
-        if(e.keyCode == 13) {
-            sendMessage();
-        }
-    });
-});
 
-window.onload = function() {
+	var messages = [];
+	var socket = io.connect('http://' + configuration.serverIP + ':3700');
+	var jField = $("#field");
+	var jSendButton = $("#send");
+	var jContent = $("#content");
+	var jName = $("#name");
+
+	var sendMessage = function() {
+		if(jName.val() == "") {
+			alert("Please type your name!");
+		} else {
+			var text = jField.val();
+			socket.emit('send', { message: text, username: jName.val() });
+			jField.val("");
+		}
+	};
  
-    var messages = [];
-    var socket = io.connect('http://localhost:3700');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
-    var name = document.getElementById("name");
+	socket.on('message', function (data) {
+		if(data.message) {
+			messages.push(data);
+			var html = '';
+			for(var i=0; i<messages.length; i++) {
+				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+				html += messages[i].message + '<br />';
+			}
+			jContent[0].innerHTML = html;
+			jContent[0].scrollTop = jContent[0].scrollHeight;
+		} else {
+			console.log("There is a problem:", data);
+		}
+	});
  
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-                html += messages[i].message + '<br />';
-            }
-            content.innerHTML = html;
-            content.scrollTop = content.scrollHeight;
-        } else {
-            console.log("There is a problem:", data);
-        }
-    });
+	jField.on('keyup', function(e) {
+		if(e.keyCode == 13) {
+			sendMessage();
+		}
+	});
+
+	jSendButton.on('click', function() {
+		sendMessage();
+	});
  
-    sendButton.onclick = function() {
-        if(name.value == "") {
-            alert("Please type your name!");
-        } else {
-            var text = field.value;
-            socket.emit('send', { message: text, username: name.value });
-            field.value = "";
-        }
-    };
- 
-}
+});
